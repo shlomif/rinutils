731557315cd7c946ac905668fc4aff130848903e
Shlomi Fish <shlomif@shlomifish.org>
2020-08-19 11:14:58 +0300

    refactoring: remove unused code

--- t/lib/Rinutils/Paths.pm
+++ t/lib/Rinutils/Paths.pm
@@ -49,55 +49,11 @@ sub _is_tag
 
     return ( ( $ENV{FCS_TEST_TAGS} // '' ) =~ /\b\Q$tag\E\b/ );
 }
-my $BREAK_TAG   = _is_tag('break_backcompat');
-my $FC_ONLY     = _is_tag('fc_only');
-my $NO_FLARES   = _is_tag('no_flares');
-my $NO_PATSOLVE = _is_tag('no_pats');
-my $NO_VALGRIND = _is_tag('no_valg');
-my $NO_DBM      = _is_tag('no_dbm');
-my $DBM_APR     = _is_tag('dbm_apr');
 
 # A file in the output/binaries directory where fc-solve was compiled.
 sub bin_file
 {
     return $FCS_PATH->child( @{ shift @_ } );
-}
-
-sub dll_file
-{
-    my $fn = shift;
-    return bin_file( [ "lib$fn." . ( $IS_WIN ? "dll" : "so" ) ] );
-}
-
-sub bin_exe_raw
-{
-    return _correct_path( bin_file(@_) ) . $EXE_SUF;
-}
-
-# A board file in the binary directory.
-sub bin_board
-{
-    return $FCS_PATH->child(shift);
-}
-
-sub is_break
-{
-    return $BREAK_TAG;
-}
-
-sub is_freecell_only
-{
-    return $FC_ONLY;
-}
-
-sub is_without_flares
-{
-    return $NO_FLARES;
-}
-
-sub is_without_valgrind
-{
-    return $NO_VALGRIND;
 }
 
 sub normalize_lf
